7d4b15032fa57015c9c7f8703e1f68e6c774398d
jjy3385 <jjy3386@gmail.com>
2025-11-02 20:26:57 +0900

    fix: ci check

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { ProjectCard } from './components/ProjectCard'
 import { ProcessingDashboard } from './components/ProcessingDashboard'
 import { Button } from './components/ui/button'
