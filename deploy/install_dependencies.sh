09b850a677a893c63bfea96004fa72aec6701de2
kimhyunso <87798982+kimhyunso@users.noreply.github.com>
2025-11-10 15:22:27 +0900

    fix: 의존성 설치 배포스크립트 수정 (#93)
    * fix: 배포스크립트 npm -> pnpm 수정
    
    * fix: 배포스크립트 수정
    
    * fix: 배포 스크립트 수정

--- deploy/install_dependencies.sh
+++ deploy/install_dependencies.sh
@@ -8,9 +8,10 @@ if [ ! -d "$APP_DIR" ]; then
     echo "Directory $APP_DIR does not exist. CodeDeploy 파일 매핑을 확인하세요." >&2
     exit 1
 fi
+npm install -g pnpm
 
 cd "$APP_DIR"
 
 echo "Installing dependencies with npm ci..."
-pnpm install
+pnpm install --frozen-lockfile
 echo "Life Cycle - AfterInstall: Dependency installation complete."
